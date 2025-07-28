cb9e0184e9946dc29ae092ec6c4570649ac8b9a3
Christopher Raquet <christopher.raquet@kit.edu>
2025-07-28 09:43:51 +0200

    Update all icons

--- lib/fields/FieldIcon.ts
+++ lib/fields/FieldIcon.ts
@@ -1,17 +1,24 @@
 import { FieldImage } from "blockly"
 
+export interface FieldIconOptions {
+    tooltip?: string
+    classList?: string[]
+}
+
 export class FieldIcon extends FieldImage {
-    constructor(
-        icon: string,
-        callback: () => void,
-        opts?: { tooltip?: string },
-    ) {
+    opts: FieldIconOptions
+
+    constructor(icon: string, callback: () => void, opts?: FieldIconOptions) {
         super(icon, 16, 16, opts?.tooltip, callback)
+        this.opts = opts ?? {}
     }
 
     initView() {
         super.initView()
 
         this.imageElement?.classList.add("base-icon")
+        if (this.opts.classList) {
+            this.imageElement?.classList.add(...this.opts.classList)
+        }
     }
 }
