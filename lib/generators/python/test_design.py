8b56ced2b90801b86633af4a44828de985a4bde1
Andreas Pfeil <andreas.pfeil.rs@gmail.com>
2025-08-14 12:17:09 +0000

    test: add tests and use them in CI

---
+++ lib/generators/python/test_design.py
@@ -0,0 +1,15 @@
+from main import RecordDesign
+
+class TestRecordDesign:
+    def test_maps_to_correct_id(self):
+        id = "whatever"
+        design = RecordDesign().setId(lambda: id)
+        record, _rules = design.apply({})
+        assert record.getId() == id
+
+    def test_maps_attributes_correctly(self):
+        key = "my_key"
+        value = "my_value"
+        design = RecordDesign().addAttribute(key, lambda: value)
+        record, _rules = design.apply({})
+        assert record.contains((key, value))
