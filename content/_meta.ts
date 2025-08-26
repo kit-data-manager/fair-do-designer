8ae77de210e48b4adc030608cb1612684f3d9961
Christopher Raquet <christopher.raquet@kit.edu>
2025-08-26 13:57:31 +0200

    add page on data query tab

--- content/_meta.ts
+++ content/_meta.ts
@@ -3,6 +3,7 @@ import { MetaRecord } from "nextra"
 const meta: MetaRecord = {
     index: "Introduction",
     "getting-started": "Getting Started",
+    "data-queries": "Data Queries",
     blocks: "Block Manual",
     advanced: "Advanced Guides",
     openDesigner: {
