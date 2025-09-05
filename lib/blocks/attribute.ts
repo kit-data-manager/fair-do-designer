c52ad16dd0294aefb7769c965867bfc8d67093f5
Andreas Pfeil <andreas.pfeil.rs@gmail.com>
2025-09-05 16:57:39 +0000

    chore: add statement neighbour typing (ignored by most blocks yet)

--- lib/blocks/attribute.ts
+++ lib/blocks/attribute.ts
@@ -1,5 +1,6 @@
 import * as Blockly from "blockly/core"
 import { addBasePath } from "next/dist/client/add-base-path"
+import { profile_hmc } from "./hmc_profile";
 
 // addBasePath("/docs/blocks/profile#additional-attributes-and-multiple-profiles")
 
@@ -10,8 +11,9 @@ export const attribute = {
       .appendField('Attribute-PID');
     this.appendValueInput('VALUE')
       .appendField(new Blockly.FieldLabelSerializable('with value'), 'VALUE');
-    this.setPreviousStatement(true, null);
-    this.setNextStatement(true, null);
+    let neighbours = ["profile_hmc", "attribute_key"]
+    this.setPreviousStatement(true, neighbours);
+    this.setNextStatement(true, neighbours);
     this.setTooltip('Additional Attribute, independent of profiles.');
     this.setHelpUrl(addBasePath("/docs/blocks/profile#additional-attributes-and-multiple-profiles"));
     this.setColour(225);
