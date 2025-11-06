6981f1e1b3f6ea56cd806e864fe4a110bbf8ff94
Christopher Raquet <christopher.raquet@kit.edu>
2025-11-06 17:25:44 +0100

    WIP: add alert dialog provider

--- app/(designer)/layout.tsx
+++ app/(designer)/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import "../globals.css"
 import { ThemeProvider } from "@/components/ThemeProvider"
+import { AlertDialogProvider } from "@/components/ui/alert-dialog-provider"
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -34,7 +35,7 @@ export default function RootLayout({
                     enableSystem
                     disableTransitionOnChange
                 >
-                    {children}
+                    <AlertDialogProvider>{children}</AlertDialogProvider>
                 </ThemeProvider>
             </body>
         </html>
