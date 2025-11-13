a6f988e56fe08ed58af7acac72e1a87a560385e0
Christopher Raquet <christopher.raquet@kit.edu>
2025-11-13 17:23:11 +0100

    fix: update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,27 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from "path"
+import { fileURLToPath } from "url"
+import { FlatCompat } from "@eslint/eslintrc"
+import { globalIgnores } from "eslint/config"
+import reactHooks from "eslint-plugin-react-hooks"
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+    baseDirectory: __dirname,
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    rules: {
-      "@typescript-eslint/no-unused-vars": "warn",
-    }
-  }
-];
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    reactHooks.configs.flat.recommended,
+    {
+        rules: {
+            "@typescript-eslint/no-unused-vars": "warn",
+            "react-hooks/refs": "warn",
+            "react-hooks/set-state-in-effect": "warn",
+        },
+    },
+    globalIgnores(["out/*", ".next/*"]),
+]
 
-export default eslintConfig;
+export default eslintConfig
