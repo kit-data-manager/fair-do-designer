ffe50f0597cc092bdf38917bcb14aa02d63402ef
Christopher Raquet <christopher.raquet@kit.edu>
2025-12-02 10:56:43 +0100

    add some new generated example files

--- components/InputPane.tsx
+++ components/InputPane.tsx
@@ -75,11 +75,10 @@ export function InputPane() {
     const loadExampleFiles = useCallback(async () => {
         const basePath = process.env.NEXT_PUBLIC_BASE_PATH || ""
         const urls = [
-            `${basePath}/demo/json/metadata.json`,
-            `${basePath}/demo/json/metadata(1).json`,
-            `${basePath}/demo/json/metadata(2).json`,
-            `${basePath}/demo/json/metadata(3).json`,
-            `${basePath}/demo/json/metadata(4).json`,
+            `${basePath}/demo/json/example-data1.json`,
+            `${basePath}/demo/json/example-data2.json`,
+            `${basePath}/demo/json/example-data3.json`,
+            `${basePath}/demo/json/example-data4.json`,
         ]
         const blobs: Blob[] = []
 
