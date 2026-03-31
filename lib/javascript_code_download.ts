cc8c56dc3a9d223429f560436e2511e47222dd50
Andreas Pfeil <andreas.pfeil@kit.edu>
2026-03-31 12:37:14 +0000

    fix: include boilerplate in download

--- lib/javascript_code_download.ts
+++ lib/javascript_code_download.ts
@@ -4,10 +4,27 @@ export class JavascriptCodeDownload {
     private staticFileCache: Record<string, string> = {}
 
     async downloadCodeZip(generatedCode: string) {
+        const filesToFetch = ["executor.js"]
+        const promises = filesToFetch.map((name) =>
+            name in this.staticFileCache
+                ? Promise.resolve(this.staticFileCache[name])
+                : fetch(
+                      (process.env.NEXT_PUBLIC_BASE_PATH ?? "") + "/js/" + name,
+                  ).then((res) => res.text()),
+        )
+
+        const fetchedFile = await Promise.all(promises)
+        const zippable: Record<string, Uint8Array> = {}
+
+        for (const [index, file] of fetchedFile.entries()) {
+            zippable[filesToFetch[index]] = strToU8(file)
+            this.staticFileCache[filesToFetch[index]] = file
+        }
         return new Promise<void>((resolve, reject) => {
             zip(
                 {
                     "generated.js": strToU8(generatedCode),
+                    ...zippable,
                 },
                 (err, data) => {
                     if (err) {
