738ff8c15087951187797bcf47338a22bc762ff0
Andreas Pfeil <andreas.pfeil@kit.edu>
2026-03-31 11:18:29 +0000

    fix: python generator shall stop on empty or duplicated IDs

--- public/python/conditionals.py
+++ public/python/conditionals.py
@@ -1,5 +1,25 @@
 from typing import Any, Callable, TypeGuard
 
+def is_emptyish(value: Any) -> bool:
+    """
+    Checks if a value is "empty-ish", meaning it is None, an empty string, or a string that only contains whitespace.
+    
+    :param value: The value to check.
+    :return: True if the value is empty-ish, False otherwise.
+    """
+    def is_list_any(value: object) -> TypeGuard[list[Any]]:
+        return isinstance(value, list)
+    
+    def is_tuple_any(value: object) -> TypeGuard[tuple[Any, Any]]:
+        return isinstance(value, tuple)
+
+    isNone = value is None
+    isEmptyArray = is_list_any(value) and len(value) <= 0
+    isEmptyTuple = is_tuple_any(value) and len(value) <= 0
+    isEmptyishString = isinstance(value, str) and value.strip().lower() in ("null", "", "()", "[]", "{}")
+
+    return isNone or isEmptyArray or isEmptyTuple or isEmptyishString
+
 def otherwise(either: Callable[[], Any], otherwise: Callable[[], Any]) -> Any:
     """
     Returns `either` if it is somehow a valid value, otherwise executes and returns `otherwise`.
@@ -19,19 +39,7 @@ def otherwise(either: Callable[[], Any], otherwise: Callable[[], Any]) -> Any:
     if type(eitherResult) == str and eitherResult.strip().lower() in ("null", "", "()", "[]", "{}"):
         eitherResult = None
 
-    def is_list_any(value: object) -> TypeGuard[list[Any]]:
-        return isinstance(value, list)
-    
-    def is_tuple_any(value: object) -> TypeGuard[tuple[Any, Any]]:
-        return isinstance(value, tuple)
-
-    isNone = eitherResult is None
-    isEmptyArray = is_list_any(eitherResult) and len(eitherResult) <= 0
-    isEmptyTuple = is_tuple_any(eitherResult) and len(eitherResult) <= 0
-    isEmptyishString = isinstance(eitherResult, str) and eitherResult.strip().lower() in ("null", "", "()", "[]", "{}")
-
-    isOk = not isNone and not isEmptyArray and not isEmptyTuple and not isEmptyishString
-    return eitherResult if isOk else otherwise()
+    return eitherResult if is_emptyish(eitherResult) else otherwise()
 
 def stop_with_fail(message: str | None) -> None:
     if message == None or message == "":
